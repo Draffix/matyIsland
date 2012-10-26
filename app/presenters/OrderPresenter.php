b8ebc117935c3452d744a788257d07aa9ff41b30
Draffix <jerry.klimcik@gmail.com>
2012-10-26 23:25:56 +0200

    Bring together both shipping and billing forms

--- app/presenters/OrderPresenter.php
+++ app/presenters/OrderPresenter.php
@@ -9,6 +9,15 @@ use Nette\Application\UI;
  */
 class OrderPresenter extends BasePresenter {
 
+    // pokud nic není v košíku nepovolíme přístup k objednávce
+    // a přesměrujeme ho na úvodní stránku
+    protected function startup() {
+        parent::startup();
+        if(!isset($_SESSION["cart"]) || $_SESSION["cart"]==null) {
+            $this->redirect('Homepage:');
+        }
+    }
+
     protected function createComponentShippingForm() {
         $form = new UI\Form;
         $form->addText('name', 'Jméno:')
@@ -24,34 +33,24 @@ class OrderPresenter extends BasePresenter {
         $form->addText('firmName', 'Název firmy:');
         $form->addText('ico', 'IČO:');
         $form->addText('dic', 'DIČ:');
-        $form->addSubmit('use', 'Použít');
+
+        // billing formulář pro fakturaci
+        $form->addText('bName', 'Jméno:');
+        $form->addText('bSurname', 'Příjmení:');
+        $form->addText('bTelefon', 'Telefon:');
+        $form->addText('bEmail', 'E-mail:');
+        $form->addText('bStreet', 'Ulice a č. popisné:');
+        $form->addText('bCity', 'Město:');
+        $form->addText('bPsc', 'PSČ:');
+        $form->addText('bFirmName', 'Název firmy:');
+
+        $form->addSubmit('continue', 'Pokračovat k výběru platby a dopravy');
         $form->onSuccess[] = callback($this, 'ShippingFormSubmitted');
         return $form;
     }
 
 // volá se po úspěšném odeslání registrace
     public function ShippingFormSubmitted(UI\Form $form) {
-        $values = $form->getValues();
-        $this->flashMessage('Byl jsi úspěšně zaregistrován.');
-        $this->redirect('Homepage:');
-    }
-
-    protected function createComponentBillingForm() {
-        $form = new UI\Form;
-        $form->addText('name', 'Jméno:');
-        $form->addText('surname', 'Příjmení:');
-        $form->addText('telefon', 'Telefon:');
-        $form->addText('email', 'E-mail:');
-        $form->addText('street', 'Ulice a č. popisné:');
-        $form->addText('city', 'Město:');
-        $form->addText('psc', 'PSČ:');
-        $form->addText('firmName', 'Název firmy:');
-        $form->onSuccess[] = callback($this, 'signUpFormSubmitted');
-        return $form;
-    }
-
-// volá se po úspěšném odeslání registrace
-    public function BillingFormSubmitted(UI\Form $form) {
         $values = $form->getValues();
         $this->flashMessage('Byl jsi úspěšně zaregistrován.');
         $this->redirect('Homepage:');
