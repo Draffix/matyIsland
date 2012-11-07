fcf3d1cd6d8c4a9229628fc9417e1602c16c6a16
Draffix <jerry.klimcik@gmail.com>
2012-11-07 11:55:01 +0100

    Refactoring orderPresenter
    Rename items of form, change to array from hasharray, find date and time
    and send email with data into template

--- app/presenters/OrderPresenter.php
+++ app/presenters/OrderPresenter.php
@@ -2,6 +2,9 @@
 
 use Nette\Application\UI;
 use Nette\Diagnostics\Debugger;
+use Nette\DateTime;
+use Nette\Mail\Message;
+use Nette\Templating\FileTemplate;
 
 /**
  * Description of OrderPresenter
@@ -9,7 +12,7 @@ use Nette\Diagnostics\Debugger;
  * @author Draffix
  */
 class OrderPresenter extends BasePresenter {
-    
+
     /** @var MatyIsland\OrderModel */
     private $order;
 
@@ -53,9 +56,9 @@ class OrderPresenter extends BasePresenter {
         return $form;
     }
 
-// volá se po úspěšném odeslání registrace
+    // volá se po úspěšném odeslání registrace
     public function ShippingFormSubmitted(UI\Form $form) {
-        $values = $form->getValues();
+        $values = $form->getValues(TRUE); // array
         $_SESSION["order"] = $values;
         $this->redirect('Order:paymentDelivery');
     }
@@ -74,9 +77,9 @@ class OrderPresenter extends BasePresenter {
         );
 
         $form = new UI\Form;
-        $form->addRadioList('payment', 'Placení:', $payment)
+        $form->addRadioList('cust_payment', 'Placení:', $payment)
                 ->addRule($form::FILLED);
-        $form->addRadioList('delivery', 'Doprava:', $delivery)
+        $form->addRadioList('cust_delivery', 'Doprava:', $delivery)
                 ->addRule($form::FILLED);
         $form->addSubmit('continue', 'Pokračovat k přehledu objednávky');
         $form->onSuccess[] = callback($this, 'paymentAndDeliveryFormSubmitted');
@@ -84,16 +87,14 @@ class OrderPresenter extends BasePresenter {
     }
 
     public function paymentAndDeliveryFormSubmitted(UI\Form $form) {
-        $values = $form->getValues();
-        $_SESSION["payment"] = $values->payment;
-        $_SESSION["delivery"] = $values->delivery;
-        array_push($_SESSION["cart"], $values->payment);
+        $values = $form->getValues(TRUE);
+        $_SESSION["order"] = array_merge($_SESSION["order"], $values);
         $this->redirect('Order:summary');
     }
 
     protected function createComponentCommentForm() {
         $form = new UI\Form;
-        $form->addTextArea('note', 'Vaše poznámka k objednávce:');
+        $form->addTextArea('cust_note', 'Vaše poznámka k objednávce:');
         $form->addSubmit('complete', 'Dokončit a potvrdit objednávku');
         $form->addCheckbox('agree', 'Souhlasím s podmínkami')
                 ->addRule($form::FILLED);
@@ -102,10 +103,26 @@ class OrderPresenter extends BasePresenter {
     }
 
     public function commentFormSubmitted(UI\Form $form) {
-        $values = $form->getValues();
-        
-        $this->order->saveOrder($_SESSION["values"], $_SESSION["payment"], $_SESSION["delivery"],$values->note);
-        
+        $values = $form->getValues(TRUE);
+
+        unset($values["agree"]); // odstraníme z pole zbytečný prvek checkbox
+        $dateTime = array("ord_date" => new DateTime); // zjistíme aktuální čas a datum
+
+        $_SESSION["order"] = array_merge($_SESSION["order"], $dateTime, $values); // doplníme o nové údaje
+
+        $template = $this->createTemplate();
+        $template->setFile(__DIR__ . '/../templates/Order/orderEmail.latte');
+        $template->registerFilter(new Nette\Latte\Engine);
+        $template->orderId = $this->order->saveOrder($_SESSION["order"]);
+
+        $mail = new Message;
+        $mail->setFrom('MatyLand.cz <info@matyland.com>')
+                ->addTo('jerry.klimcik@gmail.com')
+                ->setSubject('Potvrzení objednávky')
+                ->setHtmlBody($template)
+                ->send();
+
         $this->redirect('Order:complete');
     }
+
 }
