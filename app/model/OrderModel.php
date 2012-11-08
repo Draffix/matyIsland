6ad84e0f4e8e9d151e103b390e4100b838fb43ae
Draffix <jerry.klimcik@gmail.com>
2012-11-08 10:43:17 +0100

    Added method saveIntoOrderHasProduct

--- app/model/OrderModel.php
+++ app/model/OrderModel.php
@@ -19,4 +19,10 @@ class OrderModel extends Table {
         return $row->ord_id; //zjistíme ID vložené objednávky
     }
 
+    public function saveIntoOrderHasProduct($id_order, $id_product, $quantity, $price) {
+        $this->connection->query(
+                'INSERT INTO order_has_product (order_ord_id, product_prod_id, quantity, actual_price_of_product)
+                VALUES (?, ?, ?, ?)', $id_order, $id_product, $quantity, $price);
+    }
+
 }
