b218026c0e0aa721f109cd6a2501e9f49cad9aa2
Draffix <jerry.klimcik@gmail.com>
2012-12-09 23:13:24 +0100

    Added fetchOrder($id_product) method

--- app/model/OrderModel.php
+++ app/model/OrderModel.php
@@ -12,7 +12,7 @@ use Nette\DateTime;
 class OrderModel extends Table {
 
     /** @var string */
-    protected $tableName = 'order';
+    protected $tableName = 'orders';
 
     public function saveOrder($items) {
         $row = $this->connection->table($this->tableName)->insert($items);
@@ -23,6 +23,13 @@ class OrderModel extends Table {
         $this->connection->query(
                 'INSERT INTO order_has_product (order_ord_id, product_prod_id, quantity, actual_price_of_product)
                 VALUES (?, ?, ?, ?)', $id_order, $id_product, $quantity, $price);
+    }
+
+    public function fetchOrder($id) {
+        return $this->connection->query(
+                 'SELECT * FROM order_has_product AS op JOIN orders AS o ON o.ord_id = op.order_ord_id 
+                 JOIN product AS p ON p.prod_id = op.product_prod_id
+                 WHERE ord_id = ?', $id);
     }
 
 }
