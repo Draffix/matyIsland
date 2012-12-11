426cbfe133c4e17a5e82631f98831d0d2b15362f
Draffix <jerry.klimcik@gmail.com>
2012-12-11 16:12:21 +0100

    Added flash message and redirect

--- app/presenters/InfoPresenter.php
+++ app/presenters/InfoPresenter.php
@@ -5,17 +5,19 @@ use Nette\Mail\Message;
 
 class InfoPresenter extends BasePresenter {
 
+
+
     protected function createComponentContactForm() {
         $form = new UI\Form;
-        $form->addText('name', 'Jméno: *')
-                ->addRule($form::FILLED);
-        $form->addText('telefon', 'Telefon: *')
-                ->addRule($form::FILLED);
-        $form->addText('email', 'Email: *')
-                ->addRule($form::FILLED);
-        $form->addTextArea('message', 'Zpráva: *')
-                ->addRule($form::FILLED);
-        $form->addSubmit('submit', 'Odeslat');
+        $form->addText('name', 'Jméno: *');
+//                ->addRule($form::FILLED);
+        $form->addText('telefon', 'Telefon: *');
+//                ->addRule($form::FILLED);
+        $form->addText('email', 'Email: *');
+//                ->addRule($form::FILLED);
+        $form->addTextArea('message', 'Zpráva: *');
+//                ->addRule($form::FILLED);
+        $form->addSubmit('send', 'Odeslat');
 
         $form->onSuccess[] = $this->ContactFormSubmitted;
         return $form;
@@ -24,12 +26,16 @@ class InfoPresenter extends BasePresenter {
     public function ContactFormSubmitted(UI\Form $form) {
         $values = $form->getValues();
 
-        $mail = new Message;
-        $mail->setFrom('MatyLand.cz <info@matyland.com>')
-                ->addTo('jerry.klimcik@gmail.com')
-                ->setSubject('Kontaktujte nás')
-                ->setHtmlBody($values->message)
-                ->send();
+//        $mail = new Message;
+//        $mail->setFrom('MatyLand.cz <info@matyland.com>')
+//                ->addTo('jerry.klimcik@gmail.com')
+//                ->setSubject('Kontaktujte nás')
+//                ->setHtmlBody($values->message)
+//                ->send();
+
+        $this->flashMessage('Děkujeme, Vaše zpráva byla odeslána');
+
+        $this->redirect('this');
     }
 
 }
