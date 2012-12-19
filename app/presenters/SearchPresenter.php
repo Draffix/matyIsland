0d4b1442060e406be37022bcaf4c0c421f37776e
Draffix <jerry.klimcik@gmail.com>
2012-12-19 10:04:03 +0100

    Added new methods
    Added createComponentPaginator(), actionS($hledej)

--- app/presenters/SearchPresenter.php
+++ app/presenters/SearchPresenter.php
@@ -7,9 +7,20 @@
  */
 class SearchPresenter extends BasePresenter {
 
-    public function actionS($hledej) {
-        $_SESSION["search"] = $this->mainProduct->searchProduct('%' . $hledej . '%');;
-        $this->template->search = $this->mainProduct->searchProduct('%' . $hledej . '%');
+     protected function createComponentPaginator() {
+        $visualPaginator = new VisualPaginator();
+        return $visualPaginator;
+    }
+
+    public function actionS($hledej) {       
+        $this->template->word = $hledej;
+        $this->template->count = $this->mainProduct->countSearchProduct('%' . $hledej . '%')->pocet;
+
+        $paginator = $this['paginator']->getPaginator();
+        $paginator->itemsPerPage = 5;
+        $paginator->setBase(1);
+        $paginator->itemCount = $this->mainProduct->countSearchProduct('%' . $hledej . '%')->pocet;
+        $this->template->search = $this->mainProduct->searchProduct('%' . $hledej . '%', $paginator->itemsPerPage, $paginator->offset);
     }
 
 }
