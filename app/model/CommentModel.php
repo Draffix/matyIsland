9fed0ae1e675e3545b0df0a6415dc4fa7d04f238
Draffix <jerry.klimcik@gmail.com>
2012-12-22 22:54:11 +0100

    Initial commit

---
+++ app/model/CommentModel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MatyIsland;
+
+class CommentModel extends Table {
+
+    /** @var String * */
+    protected $tableName = 'comments';
+
+    public function insertComment($values) {
+        $row = $this->getTable()->insert($values);
+        return $row->com_id;
+    }
+
+    public function fetchAllComments($product_id) {
+        return $this->connection->query(
+                        'SELECT comments.*, user.user_login
+                        FROM comments, user
+                        WHERE comments.product_prod_id = ?
+                        AND user.user_id = comments.user_user_id
+                        ORDER BY comments.com_date DESC
+                        ', $product_id);
+    }
+
+    public function countAllComments($id) {
+        $row = $this->getTable()->where('product_prod_id', $id)->count();
+        return $row;
+    }
+
+}
+
