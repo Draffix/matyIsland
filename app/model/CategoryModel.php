a9a0ce21136e5299328b86be4b8756e4f920180a
Draffix <jerry.klimcik@gmail.com>
2012-12-23 19:59:03 +0100

    Added countCategoryFilter($id) and limit, offset

--- app/model/CategoryModel.php
+++ app/model/CategoryModel.php
@@ -6,14 +6,29 @@ class CategoryModel extends Table {
 
     /** @var string */
     protected $tableName = 'category';
-    
-    public function categoryFilter($id) {
+
+    public function categoryFilter($id, $limit, $offset) {
         return $this->connection->query(
-                'SELECT * FROM category, product, `category_has_product`, image 
+                        'SELECT * FROM category, product, `category_has_product`, image 
                 WHERE category.cat_id = category_has_product.category_cat_id 
                 AND product.prod_id = category_has_product.product_prod_id
                 AND product.prod_id = image.product_prod_id
-                AND cat_id = ?', $id);
+                AND cat_id = ?
+                AND product.prod_is_active = ?
+                GROUP BY product.prod_id
+                LIMIT ? OFFSET ?', $id, 1, $limit, $offset);
+    }
+
+    public function countCategoryFilter($id) {
+        return $this->connection->query(
+                        'SELECT COUNT(*) AS pocet 
+                FROM category, product, `category_has_product`, image 
+                WHERE category.cat_id = category_has_product.category_cat_id 
+                AND product.prod_id = category_has_product.product_prod_id
+                AND product.prod_id = image.product_prod_id
+                AND cat_id = ?
+                AND product.prod_is_active = ?
+                GROUP BY product.prod_id', $id, 1)->fetch();
     }
 
 }
