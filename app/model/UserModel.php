1177e2994904f482e4d2e78f8846304127a307b2
Draffix <jerry.klimcik@gmail.com>
2012-12-25 18:39:14 +0100

    Added updateHash method

--- app/model/UserModel.php
+++ app/model/UserModel.php
@@ -15,11 +15,21 @@ class UserModel extends Table {
     }
 
     public function findByName($useremail) {
-        return $this->findAll()->where('user_email', $useremail)->fetch();
+        return $this->findAll()->where('user_email', $useremail)
+                ->where('user_is_active', 1)
+                ->fetch();
     }
 
+    //zjišťujeme jestli již existuje zadaný email (pokud 0 tak neexistuje)
     public function countFindByEmail($useremail) {
         return $this->findAll()->where('user_email', $useremail)->count();
+    }
+
+    public function updateHash($hash) {
+        return $this->getTable()
+                ->where('user_hash', $hash)
+                ->update(array('user_hash' => NULL,
+                    'user_is_active' => 1));
     }
 
     public function setPassword($id, $password) {
