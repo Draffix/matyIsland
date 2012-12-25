cebe0bab42be73d4694c89f9fce6a367e174848b
Draffix <jerry.klimcik@gmail.com>
2012-12-25 12:44:58 +0100

    Uncomment sending emails

--- app/presenters/InfoPresenter.php
+++ app/presenters/InfoPresenter.php
@@ -9,14 +9,13 @@ class InfoPresenter extends BasePresenter {
 
     protected function createComponentContactForm() {
         $form = new UI\Form;
-        $form->addText('name', 'Jméno: *');
-//                ->addRule($form::FILLED);
-        $form->addText('telefon', 'Telefon: *');
-//                ->addRule($form::FILLED);
-        $form->addText('email', 'Email: *');
-//                ->addRule($form::FILLED);
-        $form->addTextArea('message', 'Zpráva: *');
-//                ->addRule($form::FILLED);
+        $form->addText('name', 'Jméno:')
+                ->addRule($form::FILLED);
+        $form->addText('telefon', 'Telefon:');
+        $form->addText('email', 'Email:')
+                ->addRule($form::FILLED);
+        $form->addTextArea('message', 'Zpráva:')
+                ->addRule($form::FILLED);
         $form->addSubmit('send', 'Odeslat');
 
         $form->onSuccess[] = $this->ContactFormSubmitted;
@@ -26,12 +25,12 @@ class InfoPresenter extends BasePresenter {
     public function ContactFormSubmitted(UI\Form $form) {
         $values = $form->getValues();
 
-//        $mail = new Message;
-//        $mail->setFrom('MatyLand.cz <info@matyland.com>')
-//                ->addTo('jerry.klimcik@gmail.com')
-//                ->setSubject('Kontaktujte nás')
-//                ->setHtmlBody($values->message)
-//                ->send();
+        $mail = new Message;
+        $mail->setFrom('MatyLand.cz <info@matyland.com>')
+                ->addTo('jerry.klimcik@gmail.com')
+                ->setSubject('Kontaktujte nás')
+                ->setHtmlBody($values->message)
+                ->send();
 
         $this->flashMessage('Děkujeme, Vaše zpráva byla odeslána');
 
