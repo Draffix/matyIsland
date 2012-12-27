43b1ced4494369fb0217685191ed19ca029df3af
Draffix <jerry.klimcik@gmail.com>
2012-12-27 12:52:08 +0100

    Check if product is on the stock

--- app/presenters/BasketPresenter.php
+++ app/presenters/BasketPresenter.php
@@ -6,9 +6,23 @@ use Nette\Application\UI;
  * Presenter pro nákupní košík
  */
 class BasketPresenter extends BasePresenter {
-    
+
+    /**
+     * @var MatyIsland\ProductModel
+     */
+    protected $products;
+
+    protected function startup() {
+        parent::startup();
+        $this->products = $this->context->product;
+    }
+
     public function renderDefault() {
-        
+        foreach (array_keys($_SESSION['cart']) as $key) {
+            if ($this->products->countProductQuantity($key)->pocet == 0) {
+                $_SESSION['cart'][$key]['prod_on_stock'] = 0;
+            }
+        }
     }
 
     public function handleRemoveFromCart($id) {
@@ -34,12 +48,19 @@ class BasketPresenter extends BasePresenter {
 
     public function updateFormSubmitted($form) {
         $values = $form->getValues(); // získám všechny hodnoty z formuláře
-        
+// zkotrolujeme, zda máme tolik kusů na skladě. Pokud ne, přidáme do košíku
+// tolik, kolik na skladě máme
+        if ($this->products->countProductQuantity($_SESSION["cart"][$values->product_id]['prod_id'])->pocet < $values->quantity) {
+            $values->quantity = $this->products->countProductQuantity($_SESSION["cart"][$values->product_id]['prod_id'])->pocet;
+            $this->flashMessage('Bohužel, takové množství na skladě nemáme. 
+                Do košíku bylo přidáno ' . $values->quantity . ' kusů', 'wrong');
+        }
+
         $_SESSION["cart"][$values->product_id]["totalPrice"] = $values->quantity * $_SESSION["cart"][$values->product_id]["prod_price"];
-        // pokud je zvoleno množství 0:
-        // odečte se v count celkové množství daného produktu
-        // v totalPrice se provede celková suma -= množství daného produktu * cena jednoho produktu
-        // zrušíme v session pole daného produktu
+// pokud je zvoleno množství 0:
+// odečte se v count celkové množství daného produktu
+// v totalPrice se provede celková suma -= množství daného produktu * cena jednoho produktu
+// zrušíme v session pole daného produktu
         if ($values->quantity == 0) {
             $_SESSION["count"] -= $_SESSION["cart"][$values->product_id]["basket_quantity"];
             $_SESSION["totalPrice"] -= ($_SESSION["cart"][$values->product_id]["basket_quantity"] * $this->basket->findPrice($values->product_id)->price);
@@ -49,21 +70,21 @@ class BasketPresenter extends BasePresenter {
             }
         }
 
-        // pokud je zvolené množství větší než stávající množství produktu:
-        // celkové množství += zadané množství - stávající množství produktu
-        // celková cena += cena jednoho produktu * (zadané množství - stávající množství)
-        // změníme množství produktu podle zadaného množství
+// pokud je zvolené množství větší než stávající množství produktu:
+// celkové množství += zadané množství - stávající množství produktu
+// celková cena += cena jednoho produktu * (zadané množství - stávající množství)
+// změníme množství produktu podle zadaného množství
         elseif ($values->quantity > $_SESSION["cart"][$values->product_id]["basket_quantity"]) {
-            // přičítám položky do košíku přes globální session
+// přičítám položky do košíku přes globální session
             $_SESSION["count"] += ($values->quantity - $_SESSION["cart"][$values->product_id]["basket_quantity"]);
             $_SESSION["totalPrice"] += $this->basket->findPrice($values->product_id)->price * ($values->quantity - $_SESSION["cart"][$values->product_id]["basket_quantity"]);
             $_SESSION["cart"][$values->product_id]["basket_quantity"] = $values->quantity;
         }
 
-        // pokud je zvolené množství menší než stávající množství produktu:
-        // celkové množství -= stávající množství produktu - zadané množství
-        // celková cena -= cena jednoho produktu * (stávající množství - zadané množství)
-        // změníme množství produktu podle zadaného množství
+// pokud je zvolené množství menší než stávající množství produktu:
+// celkové množství -= stávající množství produktu - zadané množství
+// celková cena -= cena jednoho produktu * (stávající množství - zadané množství)
+// změníme množství produktu podle zadaného množství
         elseif ($values->quantity < $_SESSION["cart"][$values->product_id]["basket_quantity"]) {
             $_SESSION["count"] -= ($_SESSION["cart"][$values->product_id]["basket_quantity"] - $values->quantity);
             $_SESSION["totalPrice"] -= $this->basket->findPrice($values->product_id)->price * ($_SESSION["cart"][$values->product_id]["basket_quantity"] - $values->quantity);
@@ -71,11 +92,11 @@ class BasketPresenter extends BasePresenter {
         }
 
         if ($this->getUser()->isLoggedIn()) {
-            if(isset($_SESSION["cart"][$values->product_id])) {
-            $this->basket->updateProduct($values->product_id, $_SESSION["cart"][$values->product_id]["basket_quantity"]);
+            if (isset($_SESSION["cart"][$values->product_id])) {
+                $this->basket->updateProduct($values->product_id, $_SESSION["cart"][$values->product_id]["basket_quantity"]);
             }
         }
-               
+
         $this->presenter->redirect('this');
     }
 
