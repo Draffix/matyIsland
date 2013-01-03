633b21fffd33742f76865e5135bc7c750f62a839
Draffix <jerry.klimcik@gmail.com>
2013-01-03 17:55:17 +0100

    Added paginator and filled renderDefault method

--- app/AdminModule/presenters/HomepagePresenter.php
+++ app/AdminModule/presenters/HomepagePresenter.php
@@ -1,18 +1,30 @@
 <?php
 
+namespace AdminModule;
+
 class HomepagePresenter extends BasePresenter {
 
-	/**
-	 * (non-phpDoc)
-	 *
-	 * @see Nette\Application\Presenter#startup()
-	 */
-	protected function startup() {
-		parent::startup();
-	}
+    /** @var \OrderModel */
+    protected $order;
 
-	public function renderDefault() {
+    protected function startup() {
+        parent::startup();
+        $this->order = $this->context->order;
+    }
 
-	}
-        
+    protected function createComponentPaginator() {
+        $visualPaginator = new \VisualPaginator();
+        return $visualPaginator;
+    }
+
+    public function renderDefault() {
+        $paginator = $this['paginator']->getPaginator();
+        $paginator->itemsPerPage = 6;
+        $paginator->setBase(1);
+        $paginator->itemCount = $this->order->countOrders();
+        $mainOrders = $this->order->fetchAllOrdersWithOffset($paginator->itemsPerPage, $paginator->offset);
+
+        $this->template->mainOrders = $mainOrders;
+    }
+
 }
