2984eb4ca81369e2268c6b7d02a82c5972a35fa1
Draffix <jerry.klimcik@gmail.com>
2013-01-07 00:27:43 +0100

    Added category service

--- app/AdminModule/presenters/BasePresenter.php
+++ app/AdminModule/presenters/BasePresenter.php
@@ -2,14 +2,24 @@
 
 namespace AdminModule;
 
+use Nette\Diagnostics\Debugger;
+
 class BasePresenter extends \Nette\Application\UI\Presenter {
 
     /** @var \OrderModel */
     protected $order;
 
+    /** @var \CategoryModel */
+    protected $category;
+
     protected function startup() {
         parent::startup();
         $this->order = $this->context->order;
+        $this->category = $this->context->category;
+    }
+
+    public function beforeRender() {
+        Debugger::barDump($_SESSION);
     }
 
     public function handleSignOut() {
