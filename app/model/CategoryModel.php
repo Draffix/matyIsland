8a1bbbcc5dd08b576da9e73b225a8e9d524456d0
Draffix <jerry.klimcik@gmail.com>
2013-01-27 21:15:06 +0100

    Added four new methods for category presenter

--- app/model/CategoryModel.php
+++ app/model/CategoryModel.php
@@ -31,7 +31,8 @@ class CategoryModel extends Table {
 
     public function fetchAllCategoryNames() {
         return $this->connection->table('category')
-                        ->select('cat_name');
+                        ->select('cat_name')
+                        ->select('cat_id');
     }
 
     public function insertProductIntoCategoryHasProduct($cat_id, $prod_id) {
@@ -71,7 +72,7 @@ class CategoryModel extends Table {
                         ->where(array('product_prod_id' => $prod_id))
                         ->delete();
     }
-    
+
     public function fetchAllCategoryNamesForProduct($id) {
         return $this->connection->query('
                         SELECT p.prod_id, c.cat_name, cp.category_cat_id 
@@ -81,9 +82,61 @@ class CategoryModel extends Table {
                         WHERE product_prod_id = ?', $id);
     }
 
+    /**
+     * Podle jména zjistí ID
+     * @param type $name
+     * @return type
+     */
     public function findCategoryID($name) {
         return $this->getTable()
                         ->select('cat_id')
                         ->where('cat_name', $name);
     }
+
+    /**
+     * Zjistíme údaje o kategorii a kolik má položek - pro výpis kategorií
+     * @return type
+     */
+    public function fetchAllCategoriesAndCountProducts() {
+        return $this->connection->query('
+                        SELECT c.cat_id, c.cat_name, COUNT(cp.product_prod_id) AS pocetPolozek
+                        FROM category AS c
+                        LEFT JOIN category_has_product AS cp ON cp.category_cat_id = c.cat_id
+                        GROUP BY `cat_id`           
+                        ');
+    }
+
+    /**
+     * Změníme název kategorie
+     * @param type $cat_id
+     * @param type $cat_name
+     * @return type
+     */
+    public function updateCategoryName($cat_id, $cat_name) {
+        return $this->getTable()
+                        ->where('cat_id', $cat_id)
+                        ->update(array('cat_name' => $cat_name));
+    }
+
+    /**
+     * Vymaže kategorii
+     * @param type $cat_id
+     * @return type
+     */
+    public function deleteCategory($cat_id) {
+        return $this->getTable()
+                        ->where('cat_id', $cat_id)
+                        ->delete();
+    }
+
+    /**
+     * Vytvoří kategorii
+     * @param type $values
+     * @return type
+     */
+    public function addCategory($values) {
+        return $this->getTable()
+                        ->insert($values);
+    }
+
 }
