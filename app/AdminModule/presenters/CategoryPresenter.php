f48586643dfeaf171dd0280062324ae430629ba9
Draffix <jerry.klimcik@gmail.com>
2013-01-27 21:18:11 +0100

    Refactored

--- app/AdminModule/presenters/CategoryPresenter.php
+++ app/AdminModule/presenters/CategoryPresenter.php
@@ -2,18 +2,50 @@
 
 namespace AdminModule;
 
+use Nette\Application\UI;
+
 class CategoryPresenter extends BasePresenter {
 
-    public function renderDefault() {
-        
+    public function handleDeleteCategory($cat_id) {
+        $this->category->deleteCategory($cat_id);
+        $this->flashMessage('Kategorie byla smazána', 'success');
+        $this->redirect('this');
     }
 
-    protected function createComponentCategoryGrid($name) {
-        $grid = new \Grido\Grid($this, $name);
-        $grid->setModel($this->category->pokus());
-        $grid->addColumn('cat_id', 'ID');
-        $grid->addFilter('cat_id', 'Birthday');
-        $grid->addColumn('cat_name', 'Jméno');
+    public function renderDefault() {
+        $this->template->categories = $this->category->fetchAllCategoriesAndCountProducts();
+    }
+
+    // upraví jméno kategorie
+    public function createComponentUpdateCategoryForm() {
+        $form = new UI\Form;
+        $form->addText('cat_name');
+        $form->addHidden('cat_id');
+        $form->addSubmit('save_change');
+        $form->onSuccess[] = callback($this, 'updateCategoryFormSubmitted');
+        return $form;
+    }
+
+    public function updateCategoryFormSubmitted(UI\Form $form) {
+        $values = $form->getValues();
+        $this->category->updateCategoryName($values->cat_id, $values->cat_name);
+        $this->flashMessage('Kategorie byla změněna', 'success');
+        $this->redirect('this');
+    }
+
+    public function createComponentAddCategoryForm() {
+        $form = new UI\Form;
+        $form->addText('cat_name');
+        $form->addSubmit('save_change');
+        $form->onSuccess[] = callback($this, 'addCategoryFormSubmitted');
+        return $form;
+    }
+
+    public function addCategoryFormSubmitted(UI\Form $form) {
+        $values = $form->getValues();
+        $this->category->addCategory($values);
+        $this->flashMessage('Kategorie byla přidána', 'success');
+        $this->redirect('this');
     }
 
 }
