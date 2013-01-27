e48c20c500f2bc7f4ac11d967efc5169c036f630
Draffix <jerry.klimcik@gmail.com>
2013-01-27 23:44:09 +0100

    Refactored for edit user

--- app/model/UserModel.php
+++ app/model/UserModel.php
@@ -8,16 +8,32 @@ class UserModel extends Table {
     /** @var string */
     protected $tableName = 'user';
 
+    /**
+     * Uloží uživatele
+     * @param type $values
+     * @return type
+     */
     public function saveUser($values) {
         return $this->getTable()->insert($values);
     }
 
+    /**
+     * Upraví uživatelovy údaje
+     * @param type $values
+     * @param type $userID
+     * @return type
+     */
     public function updateUser($values, $userID) {
         return $this->getTable()
                         ->where('user_id', $userID)
                         ->update($values);
     }
 
+    /**
+     * Podle emailu zjistí uživatelovy údaje
+     * @param type $useremail
+     * @return type
+     */
     public function findByName($useremail) {
         return $this->findAll()
                         ->where('user_email', $useremail)
@@ -27,9 +43,16 @@ class UserModel extends Table {
 
     //zjišťujeme jestli již existuje zadaný email (pokud 0 tak neexistuje)
     public function countFindByEmail($useremail) {
-        return $this->findAll()->where('user_email', $useremail)->count();
+        return $this->findAll()
+                        ->where('user_email', $useremail)
+                        ->count();
     }
 
+    /**
+     * Po úspěšné registraci vymažeme hash
+     * @param type $hash
+     * @return type
+     */
     public function updateHash($hash) {
         return $this->getTable()
                         ->where('user_hash', $hash)
@@ -37,10 +60,36 @@ class UserModel extends Table {
                             'user_is_active' => 1));
     }
 
+    /**
+     * Změní uživatelovi heslo
+     * @param type $id
+     * @param type $password
+     */
     public function setPassword($id, $password) {
-        $this->getTable()->where(array('user_id' => $id))->update(array(
-            'user_password' => Authenticator::calculateHash($password)
-        ));
+        $this->getTable()->
+                where(array('user_id' => $id))
+                ->update(array(
+                    'user_password' => Authenticator::calculateHash($password)
+                ));
+    }
+
+    /**
+     * Pro výpis všech uživatelů a jejich údajů
+     * @return type
+     */
+    public function fetchAllUsers() {
+        return $this->findAll();
+    }
+
+    /**
+     * Pro získání všech údajů jednoho uživatele
+     * @param type $user_id
+     * @return type
+     */
+    public function fetchUser($user_id) {
+        return $this->getTable()
+                        ->where('user_id', $user_id)
+                        ->fetch();
     }
 
 }
