b5504ca6d8f22584087200a3d8afbff85dbc9cec
Draffix <jerry.klimcik@gmail.com>
2013-01-28 19:07:28 +0100

    Added handle and filled renderDefault

--- app/AdminModule/presenters/HomepagePresenter.php
+++ app/AdminModule/presenters/HomepagePresenter.php
@@ -4,9 +4,30 @@ namespace AdminModule;
 
 class HomepagePresenter extends BasePresenter {
 
+    // odstraní objednávku
+    public function handleDeleteOrder($ord_id) {
+        $this->order->deleteOrder($ord_id);
+        $this->flashMessage('Objednávka byla smazána', 'success');
+        $this->redirect('this');
+    }
+
     public function renderDefault() {
-        $mainOrders = $this->order->fetchAllOrders();
-        $this->template->mainOrders = $mainOrders;
+        $this->template->mainOrders = $this->order->fetchAllOrders();
+
+        // celkový počet
+        $this->template->countOrders = $this->order->countOrders();
+        $this->template->countUsers = $this->users->countUsers();
+        $this->template->countProducts = $this->product->countProducts();
+        $this->template->countCategories = $this->category->countCategories();
+
+        // nevyřízené objednávky
+        $this->template->unfinishedOrders = $this->order->countUnfinishedOrders();
+
+        // zablokovaní uživatelé
+        $this->template->blockedUsers = $this->users->countBlockedUsers();
+
+        // neaktivní produkty
+        $this->template->inactiveProducts = $this->product->countInactiveProducts();
     }
 
 }
