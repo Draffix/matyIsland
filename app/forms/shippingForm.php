fff0b88e384850fba5a260a669d2b9f42bf12324
Draffix <jerry.klimcik@gmail.com>
2013-01-31 20:00:16 +0100

    Changed form for logged user

--- app/forms/shippingForm.php
+++ app/forms/shippingForm.php
@@ -4,8 +4,14 @@ use Nette\Application\UI;
 
 class shippingForm extends UI\Form {
 
-    function __construct() {
+    /**
+     * @var UserModel
+     */
+    protected $users;
+
+    function __construct(UserModel $users, $id = NULL) {
         parent::__construct();
+        $this->users = $users;
 
         $cust_name = '';
         $cust_surname = '';
@@ -19,69 +25,85 @@ class shippingForm extends UI\Form {
         $cust_dic = '';
 
         $cust_bname = '';
-        $cust_bsurnname = '';
+        $cust_bsurname = '';
         $cust_bstreet = '';
         $cust_bcity = '';
         $cust_bpsc = '';
 
-        if (isset($_SESSION["order"]["cust_name"])) {
-            $cust_name = $_SESSION["order"]["cust_name"];
-        }
+        if ($id != NULL) {
+            $userInfo = $this->users->fetchUser($id);
 
-        if (isset($_SESSION["order"]["cust_surname"])) {
-            $cust_surname = $_SESSION["order"]["cust_surname"];
-        }
+            $cust_name = $userInfo->user_name;
+            $cust_surname = $userInfo->user_surname;
+            $cust_telefon = $userInfo->user_telefon;
+            $cust_email = $userInfo->user_email;
+            $cust_street = $userInfo->user_street;
+            $cust_city = $userInfo->user_city;
+            $cust_psc = $userInfo->user_psc;
+            $cust_firmName = $userInfo->user_firmName;
+            $cust_ico = $userInfo->user_ico;
+            $cust_dic = $userInfo->user_dic;
+        } else {
 
-        if (isset($_SESSION["order"]["cust_telefon"])) {
-            $cust_telefon = $_SESSION["order"]["cust_telefon"];
-        }
+            if (isset($_SESSION["order"]["cust_name"])) {
+                $cust_name = $_SESSION["order"]["cust_name"];
+            }
 
-        if (isset($_SESSION["order"]["cust_email"])) {
-            $cust_email = $_SESSION["order"]["cust_email"];
-        }
+            if (isset($_SESSION["order"]["cust_surname"])) {
+                $cust_surname = $_SESSION["order"]["cust_surname"];
+            }
 
-        if (isset($_SESSION["order"]["cust_street"])) {
-            $cust_street = $_SESSION["order"]["cust_street"];
-        }
+            if (isset($_SESSION["order"]["cust_telefon"])) {
+                $cust_telefon = $_SESSION["order"]["cust_telefon"];
+            }
 
-        if (isset($_SESSION["order"]["cust_city"])) {
-            $cust_city = $_SESSION["order"]["cust_city"];
-        }
+            if (isset($_SESSION["order"]["cust_email"])) {
+                $cust_email = $_SESSION["order"]["cust_email"];
+            }
 
-        if (isset($_SESSION["order"]["cust_psc"])) {
-            $cust_psc = $_SESSION["order"]["cust_psc"];
-        }
+            if (isset($_SESSION["order"]["cust_street"])) {
+                $cust_street = $_SESSION["order"]["cust_street"];
+            }
 
-        if (isset($_SESSION["order"]["cust_firmName"])) {
-            $cust_firmName = $_SESSION["order"]["cust_firmName"];
-        }
+            if (isset($_SESSION["order"]["cust_city"])) {
+                $cust_city = $_SESSION["order"]["cust_city"];
+            }
 
-        if (isset($_SESSION["order"]["cust_ico"])) {
-            $cust_ico = $_SESSION["order"]["cust_ico"];
-        }
+            if (isset($_SESSION["order"]["cust_psc"])) {
+                $cust_psc = $_SESSION["order"]["cust_psc"];
+            }
 
-        if (isset($_SESSION["order"]["cust_dic"])) {
-            $cust_dic = $_SESSION["order"]["cust_dic"];
-        }
+            if (isset($_SESSION["order"]["cust_firmName"])) {
+                $cust_firmName = $_SESSION["order"]["cust_firmName"];
+            }
 
-        if (isset($_SESSION["order"]["cust_bName"])) {
-            $cust_bname = $_SESSION["order"]["cust_bName"];
-        }
+            if (isset($_SESSION["order"]["cust_ico"])) {
+                $cust_ico = $_SESSION["order"]["cust_ico"];
+            }
 
-        if (isset($_SESSION["order"]["cust_bSurname"])) {
-            $cust_bsurnname = $_SESSION["order"]["cust_bSurname"];
-        }
+            if (isset($_SESSION["order"]["cust_dic"])) {
+                $cust_dic = $_SESSION["order"]["cust_dic"];
+            }
 
-        if (isset($_SESSION["order"]["cust_bStreet"])) {
-            $cust_bstreet = $_SESSION["order"]["cust_bStreet"];
-        }
+            if (isset($_SESSION["order"]["cust_bName"])) {
+                $cust_bname = $_SESSION["order"]["cust_bName"];
+            }
 
-        if (isset($_SESSION["order"]["cust_bCity"])) {
-            $cust_bcity = $_SESSION["order"]["cust_bCity"];
-        }
+            if (isset($_SESSION["order"]["cust_bSurname"])) {
+                $cust_bsurname = $_SESSION["order"]["cust_bSurname"];
+            }
 
-        if (isset($_SESSION["order"]["cust_bPsc"])) {
-            $cust_bpsc = $_SESSION["order"]["cust_bPsc"];
+            if (isset($_SESSION["order"]["cust_bStreet"])) {
+                $cust_bstreet = $_SESSION["order"]["cust_bStreet"];
+            }
+
+            if (isset($_SESSION["order"]["cust_bCity"])) {
+                $cust_bcity = $_SESSION["order"]["cust_bCity"];
+            }
+
+            if (isset($_SESSION["order"]["cust_bPsc"])) {
+                $cust_bpsc = $_SESSION["order"]["cust_bPsc"];
+            }
         }
 
         $this->addText('cust_name', 'Jméno: *')
@@ -126,7 +148,7 @@ class shippingForm extends UI\Form {
         $this->addText('cust_bName', 'Jméno:')
                 ->setDefaultValue($cust_bname);
         $this->addText('cust_bSurname', 'Příjmení:')
-                ->setDefaultValue($cust_bsurnname);
+                ->setDefaultValue($cust_bsurname);
         $this->addText('cust_bStreet', 'Ulice a č. popisné:')
                 ->setDefaultValue($cust_bstreet);
         $this->addText('cust_bCity', 'Město:')
