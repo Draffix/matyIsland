01ccdd77e334842a50955891951265c019694ea3
Draffix <jerry.klimcik@gmail.com>
2013-02-06 00:35:09 +0100

    Added a few new methods

--- app/model/DeliveryPaymentModel.php
+++ app/model/DeliveryPaymentModel.php
@@ -16,12 +16,22 @@ class DeliveryPaymentModel extends Table {
         return $this->getTable();
     }
 
+    public function fetchAllEnabledDelivery() {
+        return $this->getTable()
+                        ->where('delivery_enabled', 1);
+    }
+
     /**
      * Získáme všechny typy placení
      * @return type
      */
     public function fetchAllPayment() {
         return $this->connection->table($this->paymentTable);
+    }
+
+    public function fetchAllEnabledPayment() {
+        return $this->connection->table($this->paymentTable)
+                        ->where('payment_enabled', 1);
     }
 
     /**
@@ -83,6 +93,56 @@ class DeliveryPaymentModel extends Table {
                         ->where('payment_name', $payment_name)
                         ->select('payment_id')
                         ->fetch();
+    }
+
+    /**
+     * Změní popis a cenu placení
+     * @param type $payment_id
+     * @param type $describe
+     * @param type $price
+     */
+    public function updatePayment($payment_id, $describe, $price) {
+        $this->connection->table($this->paymentTable)
+                ->where('payment_id', $payment_id)
+                ->update(array('payment_describe' => $describe,
+                    'payment_price' => $price));
+    }
+
+    public function deactivatePayment($payment_id) {
+        $this->connection->table($this->paymentTable)
+                ->where('payment_id', $payment_id)
+                ->update(array('payment_enabled' => 0));
+    }
+
+    public function activatePayment($payment_id) {
+        $this->connection->table($this->paymentTable)
+                ->where('payment_id', $payment_id)
+                ->update(array('payment_enabled' => 1));
+    }
+
+    /**
+     * Změní popis a cenu dopravy
+     * @param type $delivery_id
+     * @param type $describe
+     * @param type $price
+     */
+    public function updateDelivery($delivery_id, $describe, $price) {
+        $this->getTable()
+                ->where('delivery_id', $delivery_id)
+                ->update(array('delivery_describe' => $describe,
+                    'delivery_price' => $price));
+    }
+
+    public function deactivateDelivery($delivery_id) {
+        $this->getTable()
+                ->where('delivery_id', $delivery_id)
+                ->update(array('delivery_enabled' => 0));
+    }
+
+    public function activateDelivery($delivery_id) {
+        $this->getTable()
+                ->where('delivery_id', $delivery_id)
+                ->update(array('delivery_enabled' => 1));
     }
 
 }
