1d4cfb4bd9086d685345b6b3ffa1183ffb5d0324
Draffix <jerry.klimcik@gmail.com>
2013-02-06 00:35:57 +0100

    Refactored functions

--- app/AdminModule/presenters/SettingPresenter.php
+++ app/AdminModule/presenters/SettingPresenter.php
@@ -8,6 +8,30 @@ use Nette\Utils\Validators;
 
 class SettingPresenter extends BasePresenter {
 
+    public function handleDeactivatePayment($payment_id) {
+        $this->deliveryPayment->deactivatePayment($payment_id);
+        $this->flashMessage('Nastavení bylo změněno', 'success');
+        $this->redirect('this');
+    }
+
+    public function handleActivatePayment($payment_id) {
+        $this->deliveryPayment->activatePayment($payment_id);
+        $this->flashMessage('Nastavení bylo změněno', 'success');
+        $this->redirect('this');
+    }
+
+    public function handleDeactivateDelivery($delivery_id) {
+        $this->deliveryPayment->deactivateDelivery($delivery_id);
+        $this->flashMessage('Nastavení bylo změněno', 'success');
+        $this->redirect('this');
+    }
+
+    public function handleActivateDelivery($delivery_id) {
+        $this->deliveryPayment->activateDelivery($delivery_id);
+        $this->flashMessage('Nastavení bylo změněno', 'success');
+        $this->redirect('this');
+    }
+
     public function renderDefault() {
         $this->template->setting = $this->setting->fetchAllSettings();
         $this->template->owner = $this->setting->fetchAllOwner();
@@ -50,14 +74,101 @@ class SettingPresenter extends BasePresenter {
                 return;
             } else {
                 $this->moveImage($values->folder, $values->eshop_favicon);
+                $values->eshop_favicon = $values->eshop_favicon->getSanitizedName();
+                $this->setting->updateFavicon($values->eshop_favicon);
             }
         }
 
         unset($values->folder);
-        $img = array('.jpg', '.jpeg', '.gif', '.png');
-        $values->eshop_favicon = str_replace($img, '.ico', $values->eshop_favicon->getSanitizedName());
         $this->setting->updateSetting($values);
 
+        $this->flashMessage('Nastavení bylo změněno', 'success');
+        $this->redirect('this');
+    }
+
+    public function createComponentOwnerForm() {
+        $form = new UI\Form();
+        $form->addText('owner_name');
+        $form->addText('owner_name2');
+        $form->addText('owner_email');
+        $form->addText('owner_telefon');
+        $form->addText('owner_telefon2');
+        $form->addText('owner_street');
+        $form->addText('owner_city');
+        $form->addText('owner_psc');
+        $form->addText('owner_ico');
+        $form->addText('owner_dic');
+        $form->addSubmit('save_change');
+        $form->onSuccess[] = callback($this, 'ownerFormSubmitted');
+        return $form;
+    }
+
+    public function ownerFormSubmitted(UI\Form $form) {
+        $values = $form->getValues();
+        $this->setting->updateOwner($values);
+        $this->flashMessage('Nastavení bylo změněno', 'success');
+        $this->redirect('this');
+    }
+
+    public function createComponentPaymentForm() {
+        $callback = callback($this, 'paymentFormSubmitted');
+        return new UI\Multiplier(function ($payment_id) use ($callback) {
+                            $form = new UI\Form();
+                            $form->addHidden('payment_id', $payment_id);
+                            $form->addText('payment_describe');
+                            $form->addText('payment_price');
+                            $form->addSubmit('save_change');
+                            $form->onSuccess[] = $callback;
+                            return $form;
+                        });
+    }
+
+    public function paymentFormSubmitted(UI\Form $form) {
+        $values = $form->getValues();
+        $this->deliveryPayment->updatePayment($values->payment_id, $values->payment_describe, $values->payment_price);
+        $this->flashMessage('Nastavení bylo změněno', 'success');
+        $this->redirect('this');
+    }
+
+    public function createComponentDeliveryForm() {
+        $callback = callback($this, 'deliveryFormSubmitted');
+        return new UI\Multiplier(function ($delivery_id) use ($callback) {
+                            $form = new UI\Form();
+                            $form->addHidden('delivery_id', $delivery_id);
+                            $form->addText('delivery_describe');
+                            $form->addText('delivery_price');
+                            $form->addSubmit('save_change');
+                            $form->onSuccess[] = $callback;
+                            return $form;
+                        });
+    }
+
+    public function deliveryFormSubmitted(UI\Form $form) {
+        $values = $form->getValues();
+        $this->deliveryPayment->updateDelivery($values->payment_id, $values->payment_describe, $values->payment_price);
+        $this->flashMessage('Nastavení bylo změněno', 'success');
+        $this->redirect('this');
+    }
+
+    public function createComponentDiscountForm() {
+        $form = new UI\Form();
+        $form->addText('eshop_discount');
+        $form->addCheckbox('eshop_discount_for_registered', 'Jen pro registrované uživatele')
+                ->setDefaultValue($this->setting->fetchAllSettings()->eshop_discount_for_registered);
+        $form->addSubmit('save_change');
+        $form->onSuccess[] = callback($this, 'discountFormSubmitted');
+        return $form;
+    }
+
+    public function discountFormSubmitted(UI\Form $form) {
+        $values = $form->getValues();
+
+        if ($values->eshop_discount < 0) {
+            $this->flashMessage('Byla špatně zadána sleva', 'error');
+            return;
+        }
+
+        $this->setting->updateDiscount($values);
         $this->flashMessage('Nastavení bylo změněno', 'success');
         $this->redirect('this');
     }
@@ -65,7 +176,7 @@ class SettingPresenter extends BasePresenter {
     private function checkIfImageAlredyExists($folder, $name) {
         $exists = FALSE;
         $filename = $name->getSanitizedName();
-        $targetPath = $this->context->params['wwwDir'] . '/images/info/';
+        $targetPath = $this->context->params['wwwDir'] . '/images/info/favicon/';
         if ($folder !== '') {
             $targetPath .= "/$folder";
         }
@@ -78,7 +189,7 @@ class SettingPresenter extends BasePresenter {
 
     private function moveImage($folder, $name) {
         $filename = $name->getSanitizedName();
-        $targetPath = $this->context->params['wwwDir'] . '/images/info/';
+        $targetPath = $this->context->params['wwwDir'] . '/images/info/favicon/';
         if ($folder !== '') {
             $targetPath .= "/$folder";
         }
