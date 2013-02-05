d66ddb7d4ccb147a5841966cb54e86bae4bf024c
Draffix <jerry.klimcik@gmail.com>
2013-02-06 00:34:53 +0100

    Added a few new methods

--- app/model/SettingModel.php
+++ app/model/SettingModel.php
@@ -23,6 +23,33 @@ class SettingModel extends Table {
     public function updateSetting($values) {
         $this->getTable()
                 ->where('ID', 1)
+                ->update(array('eshop_name' => $values->eshop_name,
+                    'eshop_describe' => $values->eshop_describe,
+                    'eshop_key_words' => $values->eshop_key_words,
+                    'eshop_product_on_homepage' => $values->eshop_product_on_homepage));
+    }
+
+    public function updateFavicon($values) {
+        $this->getTable()
+                ->where('ID', 1)
+                ->update(array('eshop_favicon' => $values));
+    }
+
+    public function updateOwner($values) {
+        $this->connection->table($this->ownerTable)
+                ->where('ID', 1)
+                ->update($values);
+    }
+
+    public function updateDiscount($values) {
+        $this->getTable()
+                ->where('ID', 1)
+                ->update($values);
+    }
+
+    public function updateWarning($values) {
+        $this->getTable()
+                ->where('ID', 1)
                 ->update($values);
     }
 
